92b74d57971a801d47449f5981c1d13f80652325
Aaron Nelson <aaron@aaronmakingart.com>
2020-04-07 17:17:51 -0400

    Create Intro.py

---
+++ Intro Stuff/Intro.py
@@ -0,0 +1,17 @@
+#Print Statements:
+print "Hello Rhino"
+print "Hello Python"
+
+#Variables:
+
+x = 10
+y = 20.0
+z = x + y
+
+print "Z:", z
+print type(z)
+
+name = "Aaron"
+
+print name
+print type(name)
