6807e539749b813d33b56ebf8aaf0dfa74d539f8
Aaron Nelson <aaron@aaronmakingart.com>
2020-04-08 13:03:01 -0400

    Update 3.List of Points and Curve Types.py

--- Intro Stuff/3.List of Points and Curve Types.py
+++ Intro Stuff/3.List of Points and Curve Types.py
@@ -1,26 +1,29 @@
 # Lists of Points and Curve Types
 
+#import the Rhinoscript Library and set the reference to 'rs'
 import rhinoscriptsyntax as rs
 
+#create and empty list to store points
 ptList = []
 
+#prompt user to select points
 ptList = rs.GetPoints(True, True, "Please select start point", "Select next point, Enter When finished")
 
-
+#print contents of list
 print(ptList)
 
-#Some Curves from our Point List
+#Create some Curves of different types from our Point List
 myPolyLine = rs.AddPolyline(ptList)
 myCurve = rs.AddCurve(ptList)
 myIntpCurve = rs.AddInterpCurve(ptList)
 
+#output type and GUID
 print type(myPolyLine)
 print myCurve
 print myIntpCurve
 
 #Color Curves
 #Colors are tuples
-
 color01 = (255, 0, 0) #red
 color02 = (0, 255, 0) #green
 color03 = (0, 0, 255) #blue
@@ -30,7 +33,7 @@ rs.ObjectColor(myPolyLine, color01)
 rs.ObjectColor(myCurve, color02)
 rs.ObjectColor(myIntpCurve, color03)
 
-#Add the points
+#Add the points into the Rhino document, points in the point list are just point3D objects
 for pt in ptList:
     rs.AddPoint(pt)
 
