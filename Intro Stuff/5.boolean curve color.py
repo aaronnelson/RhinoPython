d07fed8a8642d36da4b68c58bed7fc02eb746603
Aaron Nelson <aaron@aaronmakingart.com>
2020-04-08 17:33:27 -0400

    Update 5.boolean curve color.py

--- Intro Stuff/5.boolean curve color.py
+++ Intro Stuff/5.boolean curve color.py
@@ -14,19 +14,19 @@ color02 = (255, 0, 255)
 #For loops with frange(start, stop, step)
 for x in rs.frange(0.0, 10.0, 0.1):
 
-	#create the list here, we create a new list once we exhaust Y coords. to create individual sets of points in the Y Direction
+    #create the list here, we create a new list once we exhaust Y coords. to create individual sets of points in the Y Direction
     ptsForCurve = []
     for y in rs.frange(0.0, 10.0, 0.1):
-		#Z coords based on product of sines
+	#Z coords based on product of sines
         z = math.sin(x) * math.sin(y)
         
-		#store each point as a tuple with X, Y, and Z and append to the point list
+	#store each point as a tuple with X, Y, and Z and append to the point list
         pt = (x, y, z)
         ptsForCurve.append(pt)
-	#once we have all the points, create a curve using those points and add it to the Rhino Document
+    #once we have all the points, create a curve using those points and add it to the Rhino Document
     curve = rs.AddCurve(ptsForCurve)
 	
-	#Alternating colors using boolean switch
+    #Alternating colors using boolean switch
     if flip:
         rs.ObjectColor(curve, color01)
         flip = not flip
